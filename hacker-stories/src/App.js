687bf06f50625bfa83658dec40bdf54996f6afd7
carlinjshaw <carlinjshaw@gmail.com>
2023-01-23 20:23:32 -0500

    added delay is data retreival and loading sign; conditional rendering

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,55 +1,76 @@
 // import logo from './logo.svg';
-import './App.css';
-import * as React from 'react';
+import "./App.css";
+import * as React from "react";
 
 const useStorageState = (key, initialState) => {
   const [value, setValue] = React.useState(
     localStorage.getItem(key) || initialState
-    );
+  );
 
-    React.useEffect(() => {
-      localStorage.setItem(key, value)
-    }, [value, key])
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
 
-    return [value, setValue]
-}
+  return [value, setValue];
+};
 
 const App = () => {
   const initialStories = [
     {
-      title: 'React',
-      url: 'https://reactjs.org',
-      author: 'Jordan Walke',
+      title: "React",
+      url: "https://reactjs.org",
+      author: "Jordan Walke",
       num_comments: 3,
       points: 4,
       objectID: 0,
     },
     {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
       num_comments: 2,
       points: 5,
       objectID: 1,
-    }
-  ]
+    },
+  ];
   //A
-  const [stories, setStories] = React.useState(initialStories)
+  const getAsyncStories = () =>
+    new Promise((resolve) =>
+      setTimeout(() => resolve({ data: { stories: initialStories } }), 2000)
+    );
 
-const [searchTerm, setSearchTerm] = useStorageState('search' ,'React')
+  const [stories, setStories] = React.useState(initialStories);
+  const [isLoading, setIsLoading] = React.useState(false)
+  const [isError, setIsError] = React.useState(false)
+  const [searchTerm, setSearchTerm] = useStorageState('search' ,'React')
+  // const [searchTerm, setSearchTerm] = useStorageState("React");
+
+
+
+  React.useEffect(() => {
+    setIsLoading(true)
+
+    getAsyncStories().then((result) => {
+      setStories(result.data.stories);
+      setIsLoading(false)
+    })
+    .catch(()=> setIsError(true))
+  }, []);
 
   const handleSearch = (event) => {
-    setSearchTerm(event.target.value)
-  }
+    setSearchTerm(event.target.value);
+  };
 
   const handleDelete = (item) => {
-    const newStories = stories.filter((story) => item.objectID !==story.objectID)
+    const newStories = stories.filter(
+      (story) => item.objectID !== story.objectID
+    );
     setStories(newStories);
-  }
+  };
 
-  const searchedStories = stories.filter((story) => 
+  const searchedStories = stories.filter((story) =>
     story.title.toLowerCase().includes(searchTerm.toLowerCase())
-  )
+  );
 
   return (
     <div>
@@ -62,90 +83,110 @@ const [searchTerm, setSearchTerm] = useStorageState('search' ,'React')
         onInputChange={handleSearch}
       >
         <strong>Search: </strong>
-        </InputWithLabel>
+      </InputWithLabel>
 
       {/* <Search search = {searchTerm} onSearch={handleSearch}/> */}
 
       <hr />
-      <List list = {searchedStories} onRemoveItem ={handleDelete}/>
+
+      {isError && <p>Something went wrong ...</p>}
+
+      {isLoading ? (
+          <p>Loading ...</p>
+      ) : (
+        <List list={searchedStories} onRemoveItem={handleDelete} />
+      )}
+
     </div>
   );
-}
+};
 
-const InputWithLabel = ({id, label, value, type='text', onInputChange, isFocused, children}) => {
+const InputWithLabel = ({
+  id,
+  label,
+  value,
+  type = "text",
+  onInputChange,
+  isFocused,
+  children,
+}) => {
   //A
   const inputRef = React.useRef();
 
   //C
   React.useEffect(() => {
-    if(isFocused && inputRef.current) {
-      inputRef.current.focus()
+    if (isFocused && inputRef.current) {
+      inputRef.current.focus();
     }
-  }, [isFocused])
+  }, [isFocused]);
 
- return (
-  <>
-    <label htmlFor={id}>{children}</label>
-    &nbsp;
-    {/* B */}
-    <input
-      ref = {inputRef}
-      id={id}
-      type={type}
-      value={value}
-      autoFocus = {isFocused}
-      onChange={onInputChange}
-    />
-  </>
-)}
+  return (
+    <>
+      <label htmlFor={id}>{children}</label>
+      &nbsp;
+      {/* B */}
+      <input
+        ref={inputRef}
+        id={id}
+        type={type}
+        value={value}
+        autoFocus={isFocused}
+        onChange={onInputChange}
+      />
+    </>
+  );
+};
 
-const List = ({list, onRemoveItem}) => {
-  
+const List = ({ list, onRemoveItem }) => {
   // const myList = [1,2,3,4]
   return (
     <ul>
-        {list.map((item) => {
-          return <Item onRemoveItem={onRemoveItem} key = {item.objectID} item ={item}/>
-        })}
+      {list.map((item) => {
+        return (
+          <Item onRemoveItem={onRemoveItem} key={item.objectID} item={item} />
+        );
+      })}
     </ul>
-  )
-}
+  );
+};
 
-const Item = ({item, onRemoveItem}) => {
+const Item = ({ item, onRemoveItem }) => {
   const handleRemoveItem = () => {
-    onRemoveItem(item)
-  }
+    onRemoveItem(item);
+  };
 
   return (
-  <li>
-    <span>
-      <a href = {item.url}> {item.title}</a>
-    </span>
-    <span> {item.author}</span>
-    <span> {item.num_contents}</span>
-    <span> {item.points} </span>
-    <button type="button" onClick={handleRemoveItem}>Remove</button>
-  </li>
-)}
+    <li>
+      <span>
+        <a href={item.url}> {item.title}</a>
+      </span>
+      <span> {item.author}</span>
+      <span> {item.num_contents}</span>
+      <span> {item.points} </span>
+      <button type="button" onClick={handleRemoveItem}>
+        Remove
+      </button>
+    </li>
+  );
+};
 
-const Search = ({search, onSearch}) =>  (
-<>
-  <label htmlFor = "search">Search: </label>
-  <input id= "search" type = "text" value={search} onChange= {onSearch}/>
+const Search = ({ search, onSearch }) => (
+  <>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" value={search} onChange={onSearch} />
 
-  {/* <p>
+    {/* <p>
     Searching for <strong>{searchTerm}</strong>
   </p> */}
+  </>
+);
 
-</>
-)
+// const handleChange = (event) => {
+//   setSearchTerm(event.target.value)
+//   props.onSearch(event)
+// }
 
-  // const handleChange = (event) => {
-  //   setSearchTerm(event.target.value)
-  //   props.onSearch(event)
-  // }
-
-  // return(
+// return(
 // }
 
 export default App;
